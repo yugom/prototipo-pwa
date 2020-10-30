96e40cc8beea4a30005e917ffa02a376b35756e8
Yugo Miyahara <yugo.miyahara@taqtile.com.br>
2020-10-29 22:31:00 -0300

    bug styled button compiled

---
+++ src/components/atm.button/button.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {ButtonStyled} from './button.styled';
+
+export const Button = () => {
+    return(
+        <ButtonStyled>teste</ButtonStyled>
+    );
+}
---
+++ src/components/atm.button/button.styled.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const ButtonStyled = styled.button`
+    width: 300px;
+    background-color: red;
+`;
