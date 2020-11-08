30dcad22c4f05daad6dba3f2a647c21df55d0ea2
Yugo Miyahara <yugo.miyahara@taqtile.com.br>
2020-11-08 19:08:04 -0300

    change branch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Button} from './components/atm.button/button'
 export function App() {
   return (
     <div className="App">
-      <Button children='Continuar' kind='secondary' expanded={true}/>
+      <Button children='Continuar' kind='secondary' outline={true} expanded={true}/>
     </div>
   );
 }
