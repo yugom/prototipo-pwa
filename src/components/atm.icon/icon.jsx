642225ff5df7cf5fcf75d835aa0b496933be5fb7
Yugo Miyahara <yugo.miyahara@taqtile.com.br>
2020-11-19 00:09:08 -0300

    create nav header

--- src/components/atm.icon/icon.jsx
+++ src/components/atm.icon/icon.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import menu from '../../icons/ic_menu.svg'
 import cart from '../../icons/ic_cart.svg'
+import profileImg from '../../images/img_profile.png'
 
 
 export const Icon = {
     Menu: props => <img src={menu} alt="menu" width={props.size}/>,
-    Cart: props => <img src={cart} alt="menu" width={props.size}/>
+    Cart: props => <img src={cart} alt="cart" width={props.size}/>,
+    Profile: props => <img src={profileImg} alt="profile" width={props.size}/>
 }
---
+++ src/components/mol.menu/menu.styled.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const MenuStyled = styled.div`
+    height: 100vh;
+    background-color: red;
+    display: flex;
+`
+
+export const NavContainer = styled.div`
+    height: 100%;
+    background-color: #fff;
+    width: 80%;
+`
+
+export const Overlay = styled.div`
+    height: 100%;
+    background-color: black;
+    opacity: 50%;
+    width: 20%;
+`
+
