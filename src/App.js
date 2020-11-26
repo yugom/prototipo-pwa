fdbeadbce58c916272fda1a2e48f6e3f732bb04f
yugom <yugomiyahara23@gmail.com>
2020-11-25 23:59:52 -0300

    Revert "Feature/menu hamburger"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
-import { Menu } from './components/mol.menu/menu';
-import { Header } from './components/mol.header/header'
+import logo from './logo.svg';
+import './App.css';
 
-export function App() {
-  const [toggleMenu, setToggleMenu] = useState(false)
-
-  const openMenu = () => {
-    setToggleMenu(!toggleMenu)
-  }
-
+function App() {
   return (
-    <div className="App" style={{ backgroundColor: 'red' }}>
-      <Header handleMenuClick={openMenu} />
-      <Menu opened={toggleMenu} handleClick={openMenu}></Menu>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
--- src/components/atm.typography/typography.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import {FontSize, Color, FontFamily, FontWeight} from '../constants'
-
-export const H1 = styled.h1`
-    font-size: ${FontSize.Large};
-    color: ${Color.White};
-    font-family: ${FontFamily.Primary};
-    font-weight: ${FontWeight.Bold};
-`
