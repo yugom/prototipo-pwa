e41478c8531adab5f542a18f98532db996e7df52
Yugo Miyahara <yugo.miyahara@taqtile.com.br>
2020-11-26 00:00:21 -0300

    CR fix

--- src/components/mol.menu/menu.styled.jsx
+++ src/components/mol.menu/menu.styled.jsx
@@ -1,12 +1,15 @@
 import styled from 'styled-components'
 import { Color } from '../constants'
 
+const menuContainerZIndex = '1';
+const menuOverlayZIndex = '0';
+
 export const NavContainer = styled.div`
     width: 80%;
     position: fixed;
     top: 0;
     bottom: 0;
-    z-index: 10;
+    z-index: ${menuContainerZIndex};
     background-color: ${Color.White};
     transform: ${props => props.opened ? 'translateX(0)' : 'translateX(-100%)'};
     transition: all .4s ease-in-out;
@@ -18,6 +21,7 @@ export const Overlay = styled.div`
     top: 0;
     bottom: 0;
     left: 0;
+    z-index: ${menuOverlayZIndex};
     transform: ${props => props.opened ? 'translateX(0)' : 'translateX(-100%)'};
     transition: all .4s ease-in-out;
     background-color: ${Color.Black};
