7ecb46eba53d85e2dcd4debe7e6c8aa78ae70067
Yugo Miyahara <yugo.miyahara@taqtile.com.br>
2020-11-28 21:12:52 -0300

    change button children with icon

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Icon } from '../components/atm.icon/icon';
 import { H1 } from '../components/atm.typography/typography.styled'
+import { IconSize } from '../components/constants';
 import { FieldButton } from '../components/mol.field-button/field-button';
 
 
 export const Home = () => {
+    const searchIcon = <Icon.Search size={IconSize.Medium} />;
     return (
         <div>
             <H1>Home</H1>
-            <FieldButton children={'Cadastrar'} placeholder={'Escreva aqui'} />
+            <FieldButton children={searchIcon} placeholder={'Escreva aqui'} />
         </div>
     );
 }
