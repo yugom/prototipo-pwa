b5f1dd33f156ea5f1e54cde45449f950c40013c3
Yugo Miyahara <yugo.miyahara@taqtile.com.br>
2020-12-01 22:37:34 -0300

    fix FieldButton handleSubmit call

--- src/components/mol.field-button/field-button.jsx
+++ src/components/mol.field-button/field-button.jsx
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button } from '../atm.button/button';
 import { FormField } from '../atm.form-field/form-field';
 import { ButtonFieldStyled } from './field-button.styled';
 
 export const FieldButton = (props) => {
-    const [input, setInput] = useState({});
     const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(input)
+        props.handleSubmit(event);
     }
     const handleInputChange = (event) => {
-        setInput(inputs => ({ ...inputs, [event.target.name]: event.target.value }));
+        props.handleInputChange(event);
     }
     return (
         <form onSubmit={handleSubmit}>
             <ButtonFieldStyled>
                 <FormField name={props.name} placeholder={props.placeholder} flatBorder={true} handleInputChange={handleInputChange} />
-                <Button children={props.children} kind={'secondary'} expanded={false} flatBorder={true} submit={true} />
+                <Button children={props.children} kind={props.kind} expanded={false} flatBorder={true} submit={true} />
             </ButtonFieldStyled>
         </form>
 
