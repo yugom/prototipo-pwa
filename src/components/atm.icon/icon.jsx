f952c9a96f36529e742695621a602b162127cabd
Yugo Miyahara <yugo.miyahara@taqtile.com.br>
2020-12-05 22:39:04 -0300

    adjust header

--- src/components/atm.icon/icon.jsx
+++ src/components/atm.icon/icon.jsx
@@ -3,6 +3,7 @@ import menu from '../../icons/ic_menu.svg'
 import cart from '../../icons/ic_cart.svg'
 import close from '../../icons/ic_close.svg'
 import search from '../../icons/ic_search.svg'
+import account from '../../icons/ic_account.svg'
 import profileImg from '../../images/img_profile.png'
 
 
@@ -11,5 +12,6 @@ export const Icon = {
     Cart: props => <img src={cart} alt="cart" width={props.size} />,
     Close: props => <img src={close} alt="close" width={props.size} />,
     Search: props => <img src={search} alt="search" width={props.size} />,
+    Account: props => <img src={account} alt="account" width={props.size} />,
     Profile: props => <img src={profileImg} alt="profile" width={props.size} />
 }
